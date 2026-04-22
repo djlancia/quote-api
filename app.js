786763cc129989fe5aa16b91b5f43d46bde5eb30
Lancia <31668397+djlancia@users.noreply.github.com>
2026-04-21 17:53:24 -0700

    inital upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/api/quotes/random', (req, res) => {
+  res.send({
+    quote: getRandomElement(quotes)
+  });
+});
+
+app.get('/api/quotes', (req, res) => {
+  const person = req.query.person;
+  if (person) {
+    const quotesByPerson = quotes.filter(quote => quote.person.toLowerCase() === person.toLowerCase());
+    if (quotesByPerson.length > 0) {
+      res.send({
+        quotes: quotesByPerson
+      });
+    } else {
+      res.status(404).send({
+        error: `No quotes found for person: ${person}`
+      });
+    }
+  } else {
+    res.send({
+      quotes: quotes
+    });
+  }
+}); 
+
+app.post('/api/quotes', (req, res) => {
+  const newquote = req.query;
+  if (newquote.quote && newquote.person) {
+    quotes.push(newquote);
+    res.status(201).send({
+      quote: newquote
+    });
+  } else {
+    res.status(400).send({
+      error: 'Request body must contain quote and person'
+    });
+  }
+});
+
+
+// export app for use in main.js and for testing
+module.exports = {
+  app
+};
+
+
---
+++ main.js
@@ -0,0 +1,10 @@
+const {
+  app
+} = require("./app.js");
+
+// designate which PORT the server will listen on
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+}); // start the server and have it listen on the designated PORT
