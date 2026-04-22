09cd90ca267b70cbbfbdb38a3548c13ffb06f375
Lancia <31668397+djlancia@users.noreply.github.com>
2026-04-22 00:34:17 -0700

    Added delete quote

---
+++ public/delete-quote.js
@@ -0,0 +1,21 @@
+const deleteButton = document.getElementById('delete-quote');
+
+deleteButton.addEventListener('click', () => {
+  const quote = document.getElementById('quote').value;
+  const person = document.getElementById('person').value;
+
+  fetch(`https://quote-api-puce.vercel.app/api/quotes?quote=${quote}&person=${person}`, {
+    method: 'DELETE',
+  })
+  .then(response => response.json())
+  .then(({quote}) => {
+    const deletedQuote = document.createElement('div');
+    deletedQuote.innerHTML = `
+    <h3>Quote deleted successfully!</h3>
+    <div class="quote-text">${quote.quote}</div>
+    <div class="attribution">- ${quote.person}</div>
+    <p>Go to the <a href="index.html">home page</a> to request and view all quotes.</p>
+    `
+    newQuoteContainer.appendChild(deletedQuote);
+  });
+});
