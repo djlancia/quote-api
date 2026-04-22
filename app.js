9917e168846459c7e820b31173cb31578764f0fe
Lancia <31668397+djlancia@users.noreply.github.com>
2026-04-21 19:27:59 -0700

    show index.html

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Quote API');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 
